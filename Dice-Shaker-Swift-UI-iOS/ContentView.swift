369fe228e06419925612408aec89a7267e5fc51b
Vaibhav <mailvaibhavchopra@gmail.com>
2020-11-18 04:19:15 +0530

    Initial Commit

---
+++ Dice-Shaker-Swift-UI-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-Shaker-Swift-UI-iOS
+//
+//  Created by Vaibhav Chopra on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dice-Shaker-Swift-UI-iOS/Dice_Shaker_Swift_UI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Shaker_Swift_UI_iOSApp.swift
+//  Dice-Shaker-Swift-UI-iOS
+//
+//  Created by Vaibhav Chopra on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Shaker_Swift_UI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
